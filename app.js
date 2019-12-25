cd858ae012c9371e7aabc0615e3384d90f2f78b2
ddz <ddzyan@163.com>
2019-12-25 12:28:33 +0800

    添加测试代码

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class AppBootHook {
+  constructor(app) {
+    this.app = app;
+  }
+
+  configWillLoad() {
+    // Ready to call configDidLoad,
+    // Config, plugin files are referred,
+    // this is the last chance to modify the config.
+    console.log('configWillLoad config :', this.app.configTest);
+  }
+
+  configDidLoad() {
+    // Config, plugin files have been loaded.
+    console.log('configDidLoad config :', this.app.configTest);
+  }
+
+  async didLoad() {
+    // All files have loaded, start plugin here.
+  }
+
+  async willReady() {
+    // All plugins have started, can do some thing before app ready
+  }
+
+  async didReady() {
+    // Worker is ready, can do some things
+    // don't need to block the app boot.
+  }
+
+  async serverDidReady() {
+    // Server is listening.
+  }
+
+  async beforeClose() {
+    // Do some thing before app close.
+  }
+}
+
+module.exports = AppBootHook;
